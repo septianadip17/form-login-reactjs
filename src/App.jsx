613757979a25c2a317c310b41ad02e516d68be47
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-27 11:36:08 +0700

    input field and app jsx edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SocialLogin from "./components/SocialLogin";
+import InputField from "./components/InputField";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="login-container">
+      <h2 className="form-title">Log in with</h2>
+      <SocialLogin />
+
+      <p className="separator"><span>or</span></p>
+
+      <form action="#" className="login-form">
+        <InputField type="email" placeholder="Email address" icon="mail" />
+        <InputField type="password" placeholder="Password" icon="lock" />
+
+        <a href="#" className="forgot-password-link">Forgot password?</a>
+        <button type="submit" className="login-button">Log In</button>
+      </form>
+
+      <p className="signup-prompt">
+        Don&apos;t have an account? <a href="#" className="signup-link">Sign up</a>
       </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
