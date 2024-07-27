3b15f42d8c5d1446157026a58c0a7170780c58e4
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-27 11:34:36 +0700

    social login added

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
 const SocialLogin = () => {
   return (
-    <div>SocialLogin</div>
-  )
-}
+    <div className="social-login">
+      <button className="social-button">
+        <img src="google.svg" alt="Google" className="social-icon" />
+        Google
+      </button>
+      <button className="social-button">
+        <img src="apple.svg" alt="Apple" className="social-icon" />
+        Apple
+      </button>
+    </div>
+  );
+};
 
-export default SocialLogin
+export default SocialLogin;
